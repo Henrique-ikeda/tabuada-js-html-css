caeaf20b2e04335e0ad6a55e5cb307dcbcaf7375
Henrique kendi Ikeda <riqueikeda2004@gmail.com>
2025-05-09 14:30:52 -0300

    função de multiplicar, e printar

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,36 @@
 const multiplicationForm = document.querySelector("#multiplication-form");
 const numberInput = document.querySelector("#number");
 const multiplicationInput = document.querySelector("#multiplicator");
+const multiplicationTitle = document.querySelector(".multiplication-title span")
+const multiplicationTable = document.querySelector("#multiplication-operations");
 
 //-------------------------------------------------------------------------------
+//função para multiplicar e aparecer na tela
+const createTable = (number, multiplicatorNumber)=>{
+    multiplicationTable.innerHTML="";
+        for(i = 1;i <= multiplicatorNumber; i++){
 
+            const result = number * i
+            
+            const template = `<div class="row">
+                <div class="operation">${number} x ${i} = </div>
+                <div class="result">${result}</div>
+                </div>`;
+
+            const parser = new DOMParser();
+
+            const htmlTemplate = parser.parseFromString(template,"text/html");
+
+            const row = htmlTemplate.querySelector(".row");
+
+            multiplicationTable.appendChild(row);
+        }
+
+        multiplicationTitle.innerText = number;
+};
+
+//-------------------------------------------------------------------------------
+//evento para validação
 multiplicationForm.addEventListener("submit",(e)=>{
     e.preventDefault();
 
@@ -15,5 +42,6 @@ multiplicationForm.addEventListener("submit",(e)=>{
         return
     } /*ao fazer isso, o botão nao tera resposta se nao tiver algum resultado*/ 
 
-    console.log(multiplicationNumber,multiplicatorNumber);
+    createTable(multiplicationNumber,multiplicatorNumber);
 })
+
